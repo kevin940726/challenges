525baab67b37abebc0036016a12900242157335c
Kai Hao <kevin830726@gmail.com>
2019-12-30 23:58:26 +0800

    Use github actions

---
+++ .github/actions/codesandbox/main.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function main() {
+  try {
+    const githubToken = core.getInput('github-token', { required: true });
+
+    const octokit = new github.GitHub(githubToken);
+    const context = github.context;
+    const issue = context.issue;
+
+    if (context.payload.action !== 'opened') {
+      console.log('No PR was opened, skipping');
+      return;
+    }
+
+    // Do nothing if its not a pr
+    if (!context.payload.pull_request) {
+      console.log(
+        'The event that triggered this action was not a pull request, skipping.'
+      );
+      return;
+    }
+
+    const pull_request = context.payload.pull_request;
+    console.log(pull_request);
+
+    await octokit.checks.create({
+      owner: issue.owner,
+      repo: issue.repo,
+      name: 'codesandbox',
+      head_sha: context.sha,
+      details_url: `https://codesandbox.io/s/github/${issue.owner}/${issue.repo}/tree/${context.sha}/challenges/example-challenge`,
+    });
+  } catch (err) {
+    core.setFailed(error.message);
+    return;
+  }
+}
+
+main();
