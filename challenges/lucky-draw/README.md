8ba949c543061fdd02b067e1a0bd01dbe4d2242e
Kai Hao <kevin830726@gmail.com>
2020-01-06 23:41:26 +0800

    Kick off lucky draw repo

---
+++ challenges/lucky-draw/README.md
@@ -0,0 +1,7 @@
+# Lucky draw
+
+See [#4](https://github.com/kevin940726/challenges/issues/4).
+
+## Reviewers
+
+- Kai Hao
---
+++ challenges/lucky-draw/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <main className="App">
+      <h1>Hello World!</h1>
+    </main>
+  );
+};
+
+export default App;
