ed11faad39015604c805bd754ee57fbadd5d3a5d
Kai Hao <kevin830726@gmail.com>
2020-11-17 21:30:38 +0800

    Implement use-counter

--- challenges/use-counter/use-counter.js
+++ challenges/use-counter/use-counter.js
@@ -1,7 +1,25 @@
 import * as React from 'react';
 
 function useCounter(initialCount = 0) {
-  // TODO
+  const [count, setCount] = React.useState(initialCount);
+  const initialCountRef = React.useRef(initialCount);
+
+  React.useEffect(() => {
+    initialCountRef.current = initialCount;
+  });
+
+  const increment = React.useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+  const decrement = React.useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, []);
+  const reset = React.useCallback(() => {
+    const initializer = initialCountRef.current;
+    setCount(typeof initializer === 'function' ? initializer() : initializer);
+  }, []);
+
+  return [count, increment, decrement, reset];
 }
 
 export default useCounter;
