4b31d3d79edc5f58a9009029cdf9736b7c402cf1
yujuiting <yujuiting@me.com>
2020-11-18 14:46:36 +0800

    use-counter

--- challenges/use-counter/use-counter.js
+++ challenges/use-counter/use-counter.js
@@ -1,7 +1,16 @@
 import * as React from 'react';
 
 function useCounter(initialCount = 0) {
-  // TODO
+  const initRef = React.useRef(initialCount);
+  React.useEffect(() => {
+    initRef.current = initialCount;
+  }, [initialCount]);
+  const [value, setValue] = React.useState(initRef.current);
+  const increment = React.useCallback(() => setValue((v) => v + 1), []);
+  const decrement = React.useCallback(() => setValue((v) => v - 1), []);
+  // assume `initialCount` will always be `number` or `() => number`
+  const reset = React.useCallback(() => setValue(initRef.current), []);
+  return [value, increment, decrement, reset];
 }
 
 export default useCounter;
